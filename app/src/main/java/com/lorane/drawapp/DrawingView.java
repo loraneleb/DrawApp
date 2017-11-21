f3c1196397c5e8240202442f2958bec082a394e4
Unknown <lorane.lebrun@gmail.com>
2017-11-21 16:42:49 -0500

    Première étape tuto finie

---
+++ app/src/main/java/com/lorane/drawapp/DrawingView.java
@@ -0,0 +1,21 @@
+package com.lorane.drawapp;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by Lorane on 21/11/2017.
+ */
+
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+    }
+}
